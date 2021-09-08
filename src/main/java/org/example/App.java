81c33b9ddadadb2797f3f9069640fae8fd92b06f
EV-02 <ayeekee@knights.ucf.edu>
2021-09-07 22:56:11 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Alyssa Yee-Kee
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) {
+        Scanner se = new Scanner(System.in);
+        System.out.println( "What is your name?" );
+
+        String name = se.nextLine();
+        System.out.println("Hello, " + name + ", nice to meet you!");
+    }
+}
